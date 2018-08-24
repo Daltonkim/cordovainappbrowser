ac81084ce6aaefce248a21c4bb12e6cf7b26e9e8
dalton <dalton.korir@incentro.com>
2018-08-24 10:41:05 +0300

    first time

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    eventUrl: '',
+    bindEvents: function() {
+    	//document.addEventListener('deviceready', this.enablePushNotifications, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('resume', this.onResume, false);
+    },
+    onResume: function() {
+    	console.log('resume evenuturl: '+app.eventUrl);
+    	if(app.eventUrl != '') {
+    		ref = app.ref;
+    		ref.executeScript({code: 'window.location.href="'+app.eventUrl+'"'});
+    		app.eventUrl = '';
+		}
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+    	var openWindow = function() {
+            var ref = cordova.InAppBrowser.open('https://www.businessleaders.nl/index.php?r=app/index', '_blank', 'location=no,zoom=no,toolbar=no');
+            ref.addEventListener('exit', function(event) { navigator.app.exitApp() });
+            var loadStop = function(event) {
+            	app.enablePushNotifications(ref);
+            	ref.removeEventListener('loadstop', loadStop);
+            };
+            ref.addEventListener('loadstop', loadStop);      	
+        };
+    	document.addEventListener("online", openWindow, false);   
+    	document.addEventListener("offline", function() {
+            alert('Er kon geen verbinding gemaakt worden.');
+            navigator.app.exitApp();
+    	}, false);
+    	if (navigator.network.connection.type != 'none') {
+    		openWindow();
+    	}
+    },
+    enablePushNotifications: function(ref) {
+    	pushNotification = window.plugins.pushNotification;
+    	
+    	onNotificationIos = function(event) {
+    		if(event.foreground == 0) {
+    			app.eventUrl = event.url;
+    			app.ref = ref;
+    			console.log(event.url);
+    			console.log(app.eventUrl);
+    			
+//    			ref.executeScript({code: 'window.location.href="'+event.url+'"'});
+    		}
+//    		else {
+//    		}
+    	};
+    	
+    	pushNotification.register(function(result) {
+    		ref.executeScript({code: 'var x = new XMLHttpRequest(); x.open("GET", "index.php?r=app/iosToken&token='+result+'", false); x.send();'});
+    	}, function(result) {
+    	}, {
+    		'badge': 'true',
+    		'sound': 'true',
+    		'alert': 'true',
+    		'ecb': 'onNotificationIos'
+    	});
+    }
+};
+
+app.initialize();
---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,49 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "phonegap-plugin-push.PushNotification",
+    "file": "plugins/phonegap-plugin-push/www/push.js",
+    "pluginId": "phonegap-plugin-push",
+    "clobbers": [
+      "PushNotification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-network-information.network",
+    "file": "plugins/cordova-plugin-network-information/www/network.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "navigator.connection",
+      "navigator.network.connection"
+    ]
+  },
+  {
+    "id": "cordova-plugin-network-information.Connection",
+    "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "Connection"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "phonegap-plugin-push": "2.1.3",
+  "cordova-plugin-console": "1.1.0",
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-plugin-network-information": "2.0.1",
+  "cordova-plugin-transport-security": "0.1.2"
+};
+// BOTTOM OF METADATA
+});
